709a108e7ea1359bff4bbe92ae02d54384172a9c
bolduc-drew <abolduc@g.clemson.edu>
2017-02-16 04:58:54 -0500

    Assignment 3 added

--- build/assignment_03/CTestTestfile.cmake
+++ build/assignment_03/CTestTestfile.cmake
@@ -1,9 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/bolduc/AUE893_Bolduc/src
-# Build directory: /home/bolduc/AUE893_Bolduc/build
+# Source directory: /home/bolduc/AUE893_Bolduc/src/assignment_03
+# Build directory: /home/bolduc/AUE893_Bolduc/build/assignment_03
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(assignment_01)
-subdirs(assignment_03)
